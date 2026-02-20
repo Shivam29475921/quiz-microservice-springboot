dddef68e18f73ecf8bc4095132deb72ce7180a23
Shivam Das <shivamdas16122002@gmail.com>
2026-02-20 19:13:28 +0530

    Add files via upload

---
+++ service-registry/src/test/java/mca/nitt/service_registry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package mca.nitt.service_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
